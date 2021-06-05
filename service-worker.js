09314d24b10e098a966d60c1eee5d65b2e4bfc33
prmahalingam <prmahalingam@gmail.com>
2021-06-05 10:31:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,22 +6,35 @@ var URLS = [
   '/pwatest/index.html'
 ]
 
+
+function networkElseCache (event) {
+  return caches.match(event.request).then(match => {
+    if (!match) { return fetch(event.request); }
+    return fetch(event.request).then(response => {
+      // Update cache.
+      caches.open(VERSION).then(cache => cache.put(event.request, response.clone()));
+      return response;
+    }) || response;
+  });
+}
+
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
   e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+    networkElseCache(e)
+    //caches.match(e.request).then(function (request) {
+    //  if (request) { // if cache is available, respond with cache
+    //    console.log('responding with cache : ' + e.request.url)
+    //    return request
+    //  } else {       // if there are no cache, try fetching request
+    //    console.log('file is not cached, fetching : ' + e.request.url)
+    //    return fetch(e.request)
+    //  }
 
       // You can omit if/else for console.log & put one line below here too.
       // return request || fetch(e.request)
-    })
+    //})
   )
 })
 
